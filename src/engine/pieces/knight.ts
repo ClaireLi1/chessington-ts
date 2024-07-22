155499ed7b26cf5ba24fd653c6fd5ba00635f940
Claire Li <Claire.Li@softwire.com>
2024-07-22 15:49:57 +0100

    Undo accidental changes to knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,7 +1,6 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -9,20 +8,6 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        const current_square: Square = board.findPiece(this);
-        const current_row: number = current_square.row;
-        const current_col: number = current_square.col;
-
-        const available_moves: Square[] = [];
-
-        if (this.player === Player.WHITE){
-            available_moves.push(new Square(current_row + 1, current_col));
-        }else{
-            available_moves.push(new Square(current_row - 1, current_col));
-        }
-
-        console.log(available_moves);
-        return available_moves
-
+        return new Array(0);
     }
 }
