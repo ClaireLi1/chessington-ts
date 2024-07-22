b4de0e0470d7fb3535236a3400626476c47ebd16
Claire Li <Claire.Li@softwire.com>
2024-07-22 16:50:54 +0100

    Complete for test Red-3

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,23 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const current_square: Square = board.findPiece(this);
+        const current_row: number = current_square.row;
+        const current_col: number = current_square.col;
+
+        const available_moves: Square[] = [];
+
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i != current_row){
+                available_moves.push(new Square(i, current_col))
+            }
+
+            if (i != current_col){
+                available_moves.push(new Square(current_row, i))
+            }
+
+        }
+
+        return available_moves
     }
 }
