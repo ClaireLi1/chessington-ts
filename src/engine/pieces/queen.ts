5a03f63571f94a616c47c73c5d8eb4fd7623e959
Claire Li <Claire.Li@softwire.com>
2024-07-22 17:35:13 +0100

    Complete for test Red-5

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,36 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const current_square: Square = board.findPiece(this);
+        const current_row: number = current_square.row;
+        const current_col: number = current_square.col;
+
+        const available_moves: Square[] = [];
+
+        const coords_sum: number = current_row + current_col;
+        const coords_diff: number = current_row - current_col;
+
+        const board_size:number = GameSettings.BOARD_SIZE
+
+        for (let i = 0; i < board_size; i++) {
+            if (i != current_row){
+                available_moves.push(new Square(i, current_col))
+
+                if (coords_sum - i >= 0 && coords_sum - i < board_size){
+                    available_moves.push(new Square(i, coords_sum - i))
+                }
+            }
+
+            if (i != current_col){
+                available_moves.push(new Square(current_row, i))
+
+                if (i + coords_diff >= 0 && i + coords_diff < board_size){
+                    available_moves.push(new Square(i + coords_diff, i))
+                }
+            }
+
+        }
+
+        return available_moves
     }
 }
