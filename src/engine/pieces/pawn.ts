d2394e84483a8cf9408345f354f814fc9564bc4a
Claire Li <Claire.Li@softwire.com>
2024-07-22 15:48:59 +0100

    Make pawns do pawn things

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,20 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const current_square: Square = board.findPiece(this);
+        const current_row: number = current_square.row;
+        const current_col: number = current_square.col;
+
+        const available_moves: Square[] = [];
+
+        if (this.player === Player.WHITE){
+            available_moves.push(new Square(current_row + 1, current_col));
+        }else{
+            available_moves.push(new Square(current_row - 1, current_col));
+        }
+
+        console.log(available_moves);
+        return available_moves
+
     }
 }
