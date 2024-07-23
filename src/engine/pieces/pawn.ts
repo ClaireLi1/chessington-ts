56c073ce7f2507921711bd641f20139817599330
Claire Li <Claire.Li@softwire.com>
2024-07-23 10:25:17 +0100

    Complet for Red-8

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -13,7 +14,9 @@ export default class Pawn extends Piece {
         const current_row: number = current_square.row;
         const current_col: number = current_square.col;
 
-        const available_moves: Square[] = [];
+        const board_size:number = GameSettings.BOARD_SIZE;
+
+        var available_moves: Square[] = [];
 
         if (this.player === Player.WHITE){
             available_moves.push(new Square(current_row + 1, current_col));
@@ -21,13 +24,22 @@ export default class Pawn extends Piece {
             available_moves.push(new Square(current_row - 1, current_col));
         }
 
-        if (current_row === 1 || current_row === 6){
-            if (this.player === Player.WHITE){
+        if (current_row === 1 && this.player === Player.WHITE) {
+            if (!board.getPiece(new Square(current_row + 1, current_col))) {
                 available_moves.push(new Square(current_row + 2, current_col));
-            }else{
+            }
+        }
+        
+        if (current_row === board_size - 2 && this.player === Player.BLACK){
+            if (!board.getPiece(new Square(current_row - 1, current_col))) {
                 available_moves.push(new Square(current_row - 2, current_col));
             }
         }
+
+
+        let condition = (item: Square) => !board.getPiece(item) == true
+
+        available_moves = available_moves.filter(item => condition(item))
 
         return available_moves
     }
