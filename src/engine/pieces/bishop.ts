e788422068605dd63efaecc58fa6bfa7ec41d788
Claire Li <Claire.Li@softwire.com>
2024-07-23 11:32:05 +0100

    Complete for Red-10

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -21,16 +21,48 @@ export default class Bishop extends Piece {
 
         const board_size:number = GameSettings.BOARD_SIZE
 
-        for (let i = 0; i < board_size; i++) {
-            if (i != current_row && coords_sum - i >= 0 && coords_sum - i < board_size){
-                available_moves.push(new Square(i, coords_sum - i))
+
+        const directions = [
+            {row_move: -1, col_move: -1}, 
+            {row_move: 1, col_move: 1}, 
+            {row_move: 1, col_move: -1}, 
+            {row_move: -1, col_move: 1} 
+        ]
+
+        for (var direction of directions){
+            let row = current_row + direction.row_move;
+            let col = current_col + direction.col_move;
+
+            while (this.isOnBoard(board_size, row, col)){
+
+                var new_square: Square = new Square(row, col)
+
+                available_moves.push(new Square(row, col))
+
+                if (!!board.getPiece(new_square)){
+                    break;
+                }
+
+                row += direction.row_move
+                col += direction.col_move
             }
 
-            if (i != current_col && i + coords_diff >= 0 && i + coords_diff < board_size){
-                available_moves.push(new Square(i + coords_diff, i))
-            }
         }
 
+    //     for (let i = 0; i < board_size; i++) {
+    //         if (i != current_row && coords_sum - i >= 0 && coords_sum - i < board_size){
+    //             available_moves.push(new Square(i, coords_sum - i))
+    //         }
+
+    //         if (i != current_col && i + coords_diff >= 0 && i + coords_diff < board_size){
+    //             available_moves.push(new Square(i + coords_diff, i))
+    //         }
+    //     }
+
         return available_moves
+    }
+
+    private isOnBoard(board_size: number, row: number, col: number){
+        return (row >= 0 && row < board_size && col >= 0 && col < board_size);
     }
 }
