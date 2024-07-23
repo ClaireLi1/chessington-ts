d764e7f76693cc34c64b011d66ab3a137b2fb263
Claire Li <Claire.Li@softwire.com>
2024-07-23 11:28:34 +0100

    Complete for Red-9

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -16,17 +16,39 @@ export default class Rook extends Piece {
 
         const available_moves: Square[] = [];
 
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            if (i != current_row){
-                available_moves.push(new Square(i, current_col))
-            }
+        const board_size: number = GameSettings.BOARD_SIZE;
 
-            if (i != current_col){
-                available_moves.push(new Square(current_row, i))
+        const directions = [
+            {row_move: -1, col_move: 0}, //left
+            {row_move: 1, col_move: 0}, //right
+            {row_move: 0, col_move: 1}, //up
+            {row_move: 0, col_move: -1} //down
+        ]
+
+        for (var direction of directions){
+            let row = current_row + direction.row_move;
+            let col = current_col + direction.col_move;
+
+            while (this.isOnBoard(board_size, row, col)){
+
+                var new_square: Square = new Square(row, col)
+
+                available_moves.push(new Square(row, col))
+
+                if (!!board.getPiece(new_square)){
+                    break;
+                }
+
+                row += direction.row_move
+                col += direction.col_move
             }
 
         }
 
         return available_moves
+    }
+
+    private isOnBoard(board_size: number, row: number, col: number){
+        return (row >= 0 && row < board_size && col >= 0 && col < board_size);
     }
 }
