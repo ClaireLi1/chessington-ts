3689747bf3d0bb975a80465f96d9846a12cb303f
Claire Li <Claire.Li@softwire.com>
2024-07-23 15:10:12 +0100

    Complete for Red-18

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import Square from '../square';
 import GameSettings from '../gameSettings';
+import King from './king';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -17,39 +18,46 @@ export default class Pawn extends Piece {
         const board_size:number = GameSettings.BOARD_SIZE;
 
         var available_moves: Square[] = [];
-
-        const row_move = 1
-        
-        var max_dist: number = 1
-
-        if ((this.player === Player.WHITE && current_row === 1) || (this.player === Player.BLACK && current_row === board_size - 2)) {
-            max_dist = 2
-        }
+        const startRow = this.player === Player.WHITE ? 1 : board_size - 2;
+        const row_move = this.player === Player.WHITE ? 1 : -1;
+        const max_dist = current_row === startRow ? 2 : 1;
 
 
-        let row = current_row;
-        let col = current_col;
-
+        // Forward moves
         for (let i = 1; i <= max_dist; i++) {
+            var new_row = current_row + i * row_move;
+        
+            var new_square: Square = new Square(new_row, current_col);
 
-            if (this.player === Player.WHITE) {
-                row += row_move
-            } else {
-                row -= row_move
-            }
-            
-
-            var new_square: Square = new Square(row, col)
-
-            if (!this.isOnBoard(board_size, row, col) || !!board.getPiece(new_square)) {
-                break
+            if (!this.isOnBoard(board_size, new_row, current_col) || !!board.getPiece(new_square)) {
+                break;
             }
 
-            available_moves.push(new_square)
+            available_moves.push(new_square);
 
         }
 
-        return available_moves
+
+        // Diagonal moves
+        const directions = [{row_move : 1, col_move : 1}, {row_move : 1, col_move : -1}]
+
+        for (var direction of directions) {
+
+            var new_row = current_row + row_move * direction.row_move;
+            var new_col = current_col + direction.col_move
+
+            if (this.isOnBoard(board_size, new_row, new_col)) {
+                var new_square: Square = new Square(new_row, new_col);
+                var piece: Piece | undefined = board.getPiece(new_square);
+                if (!!piece) {
+                    if (piece.player !== this.player && !(piece instanceof King)) {
+                        available_moves.push(new_square);
+                    }
+                }                
+            }
+        }
+
+        return available_moves;
     }
 
 }
