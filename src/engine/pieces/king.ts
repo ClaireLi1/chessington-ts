d7e59d616bbb002a4e5fd99cbe4de68b2a663092
Claire Li <Claire.Li@softwire.com>
2024-07-23 09:46:41 +0100

    Complete for Red-7

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,26 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const current_square: Square = board.findPiece(this);
+        const current_row: number = current_square.row;
+        const current_col: number = current_square.col;
+
+        const available_moves: Square[] = [];
+
+        const board_size:number = GameSettings.BOARD_SIZE
+
+        const pos_list: number[][] = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+
+        for (var position of pos_list) {
+            var new_row: number = current_row + position[0]
+            var new_col: number = current_col + position[1]
+            
+            if (new_row >= 0 && new_row < board_size && new_col >= 0 && new_col < board_size) {
+                available_moves.push(new Square(new_row, new_col))
+            }
+
+        }
+        
+        return available_moves
     }
 }
