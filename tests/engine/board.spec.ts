1ba265643313b44e4f6a445f18fb353e12c955b3
Claire Li <Claire.Li@softwire.com>
2024-07-24 13:44:36 +0100

    Add test - allow en passant move

--- tests/engine/board.spec.ts
+++ tests/engine/board.spec.ts
@@ -1,4 +1,5 @@
 import 'chai/register-should';
+import { assert } from "chai";
 import Board from '../../src/engine/board';
 import Pawn from '../../src/engine/pieces/pawn';
 import Player from '../../src/engine/player';
@@ -36,6 +37,36 @@ describe('Board', () => {
 
             // Assert
             board.findPiece(pawn).should.eql(square); // Object equivalence: different objects, same data
+        });
+
+        it('allow en passant move', () => {
+            const whitePawn = new Pawn(Player.WHITE);
+            const blackPawn = new Pawn(Player.BLACK);
+            const whiteStart = Square.at(1, 4);
+            const whiteEnd = Square.at(3, 4);
+            const blackStart = Square.at(3, 3);
+            const enPassantCaptureSquare = Square.at(2, 4);
+            const whitePawnEndPosition = Square.at(3, 4);
+
+            board.setPiece(whiteStart, whitePawn);
+            board.setPiece(blackStart, blackPawn);
+
+            whitePawn.moveTo(board, whiteEnd); 
+
+            const moves = blackPawn.getAvailableMoves(board);
+
+            moves.should.deep.include(enPassantCaptureSquare);
+
+            if (moves.some(move => move.equals(enPassantCaptureSquare))) {
+                blackPawn.moveTo(board, enPassantCaptureSquare);
+            }
+            
+            const capturedBlackPawn = board.getPiece(enPassantCaptureSquare);
+            assert.equal(capturedBlackPawn, blackPawn)
+
+            const removedWhitePawn = board.getPiece(whitePawnEndPosition);
+            assert.equal(removedWhitePawn, undefined)
+
         });
 
     });
