dbb358dc3ffdd7270a6fd665df7ececa2e721216
Claire Li <Claire.Li@softwire.com>
2024-07-24 13:45:37 +0100

    Add en passant logic in Pawn.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -6,8 +6,42 @@ import GameSettings from '../gameSettings';
 import King from './king';
 
 export default class Pawn extends Piece {
+    enpassant_flag: boolean;
+
     public constructor(player: Player) {
         super(player);
+        this.enpassant_flag = false;
+    }
+
+    public moveTo(board: Board, newSquare: Square) {
+        const currentSquare = board.findPiece(this);
+
+        if (Math.abs(newSquare.col - currentSquare.col) === 1 && board.getPiece(newSquare) === undefined) {
+            const capturedPawnSquare = new Square(currentSquare.row, newSquare.col);
+            const capturedPawn = board.getPiece(capturedPawnSquare);
+            if (capturedPawn instanceof Pawn && capturedPawn.enpassant_flag == true) {
+                board.setPiece(capturedPawnSquare, undefined);
+            }
+        }
+
+        board.movePiece(currentSquare, newSquare);
+
+        this.resetEnPassantFlags(board);
+
+        if (Math.abs(newSquare.row - currentSquare.row) === 2) {
+            this.enpassant_flag = true;
+        }
+    }
+
+    private resetEnPassantFlags(board: Board) {
+        for (let row = 0; row < GameSettings.BOARD_SIZE; row++) {
+            for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
+                const piece = board.getPiece(Square.at(row, col));
+                if (piece instanceof Pawn) {
+                    piece.enpassant_flag = false;
+                }
+            }
+        }
     }
 
     public getAvailableMoves(board: Board) {
@@ -22,8 +56,6 @@ export default class Pawn extends Piece {
         const row_move = this.player === Player.WHITE ? 1 : -1;
         const max_dist = current_row === startRow ? 2 : 1;
 
-
-        // Forward moves
         for (let i = 1; i <= max_dist; i++) {
             var new_row = current_row + i * row_move;
         
@@ -37,20 +69,24 @@ export default class Pawn extends Piece {
 
         }
 
-
-        // Diagonal moves
-        const directions = [{row_move : 1, col_move : 1}, {row_move : 1, col_move : -1}]
+        var directions = [{row_move : 1, col_move : 1}, {row_move : 1, col_move : -1}]
 
         for (var direction of directions) {
 
             var new_row = current_row + row_move * direction.row_move;
-            var new_col = current_col + direction.col_move
+            var new_col = current_col + direction.col_move;
 
             if (this.isOnBoard(board_size, new_row, new_col)) {
                 var new_square: Square = new Square(new_row, new_col);
                 var piece: Piece | undefined = board.getPiece(new_square);
                 if (!!piece) {
                     if (piece.player !== this.player && !(piece instanceof King)) {
+                        available_moves.push(new_square);
+                    }
+                } else {
+                    const capturedPawnSquare = new Square(current_row, new_col);
+                    const capturedPawn = board.getPiece(capturedPawnSquare);
+                    if (capturedPawn instanceof Pawn && capturedPawn.enpassant_flag == true) {
                         available_moves.push(new_square);
                     }
                 }                
